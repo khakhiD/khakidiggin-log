cf40efbd86fadece3324030161e4c0f8a7a0deaf
Dongho Shin <kakkiid@gmail.com>
2023-06-10 23:12:50 +0900

    style: 카테고리 '📂 All'에서 'All'로 변경

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_CATEGORY = "📂 All" as const
+export const DEFAULT_CATEGORY = "All" as const
 export const COLOR_SET: { [key: string]: string } = {
   0: "bg-sky-200",
   1: "bg-rose-200",
