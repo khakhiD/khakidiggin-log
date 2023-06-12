1bfc1706877bea3f45568308d52485e375bfaee7
Dongho Shin <kakkiid@gmail.com>
2023-06-13 02:47:59 +0900

    feat: 반응형 navbar 재구성 - vw<lg: 아이콘 메뉴 - vw>lg: 텍스트 메뉴

--- src/components/Layout/Header/NavBar.tsx
+++ src/components/Layout/Header/NavBar.tsx
@@ -1,7 +1,11 @@
+import { CONFIG } from "@/site.config"
 import Link from "next/link"
+import { AiFillLinkedin, AiFillMail, AiFillGithub } from "react-icons/ai"
 
 const NavBar: React.FC = () => {
-  const links = [{ id: 1, name: "About", to: "/about" }]
+  const links = [{ id: 1, name: "About", to: "/about" },
+                { id: 2, name: "Github", to: `https://github.com/${CONFIG.profile.github}`, icon: <AiFillGithub />},
+                { id: 3, name: "LinkedIn", to: `https://linked.in/in/${CONFIG.profile.linkedin}`, icon: <AiFillLinkedin/>}]
   return (
     <div className="flex-shrink-0">
       <ul className="flex flex-row">
@@ -11,7 +15,10 @@ const NavBar: React.FC = () => {
             className="block ml-4 text-black text-gray-500 dark:text-white nav"
           >
             <Link href={link.to}>
-              <a className="font-medium hover:font-semi-bold hover:text-sky-500 dark:font-semi-bold dark:hover:text-sky-400">{link.name}</a>
+              <a className="hidden lg:block lg:font-medium lg:hover:font-semi-bold lg:hover:text-sky-500 lg:dark:font-semi-bold lg:dark:hover:text-sky-400">{link.name}</a>
+            </Link>
+            <Link href={link.to}>
+              <a className="text-xl text-zinc-900 dark:text-zinc-300 hover:text-sky-500 dark:font-semi-bold dark:hover:text-sky-400 lg:hidden">{link.icon}</a>
             </Link>
           </li>
         ))}
