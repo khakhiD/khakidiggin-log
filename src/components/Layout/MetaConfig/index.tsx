40389db80a7290b550a09a603631a96cbb1270ef
Dongho Shin <kakkiid@gmail.com>
2023-06-12 14:57:59 +0900

    feat: metadata image changed

--- src/components/Layout/MetaConfig/index.tsx
+++ src/components/Layout/MetaConfig/index.tsx
@@ -30,7 +30,12 @@ const MetaConfig: React.FC<MetaConfigProps> = ({ ...props }) => {
       <meta property="og:description" content={meta.description} />
       <meta property="og:url" content={meta.url} />
       {CONFIG.lang && <meta property="og:locale" content={CONFIG.lang} />}
-      {meta.image && <meta property="og:image" content={meta.image} />}
+      {CONFIG.ogImageGenerateURL && <meta property="og:image" content={CONFIG.ogImageGenerateURL} />}
+      {/* metadata images
+        !meta.image && CONFIG.ogImageGenerateURL && <meta property="og:image" content={CONFIG.ogImageGenerateURL} />
+        meta.image && <meta property="og:image" content={meta.image} />
+      */}
+      
       {/* twitter */}
       <meta name="twitter:title" content={meta.title} />
       <meta name="twitter:description" content={meta.description} />
