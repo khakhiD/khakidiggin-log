5ac5b001201117e0df4f0b8c26cd99123ee4ab42
Dongho Shin <kakkiid@gmail.com>
2023-06-12 22:31:50 +0900

    style: 배경 색 변경 - light: bg-white - dark: bg-zink-900

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,7 +36,7 @@ class MyDocument extends Document {
             </>
           )}
         </Head>
-        <body className="bg-slate-100 dark:bg-gray-900">
+        <body className="bg-white dark:bg-zinc-900">
           <Main />
           <NextScript />
         </body>
