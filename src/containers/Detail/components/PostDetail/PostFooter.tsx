8545bbec44b2a8b65ccf6123fc050e6217fdb717
Dongho Shin <kakkiid@gmail.com>
2023-06-13 01:39:57 +0900

    style: <PostFooter> Author responsive style added.

--- src/containers/Detail/components/PostDetail/PostFooter.tsx
+++ src/containers/Detail/components/PostDetail/PostFooter.tsx
@@ -12,7 +12,7 @@ const Footer: React.FC<Props> = () => {
     <div className="border-t border-slate-200 dark:border-gray-500 mt-10">
       <div className="post-footer pt-12 px-4 flex">
         <div className="member-card flex min-w-fit">
-          <div className="group thumbnail-wrapper flex flex-col mr-[40px] top-0 w-20 h-20">
+          <div className="group thumbnail-wrapper flex flex-col mr-[40px] top-0 w-20 h-20 md:w-28 md:h-28 ">
             <div className="top-0 m-0 rounded-full">
               <Image
                 className="rounded-full"
@@ -27,12 +27,12 @@ const Footer: React.FC<Props> = () => {
             </div>
           </div>
           <div className="description w-[100%] flex flex-col">
-            <p className="leading-6 text-sm mt-2 text-gray-700 dark:text-gray-200 text-base">
+            <p className="leading-5 md:leading-6 text-md md:text-lg mt-2 text-gray-700 dark:text-gray-200 text-base">
               추억을 지지고 볶을 수 있는 웹 서비스를 개발하고 싶습니다.
               <br />
-              <b>사용자 중심의 프론트엔드 개발자</b> 카키디 입니다.
+              <b>사용자 중심의 프론트엔드 개발자</b> <span className="text-sky-500 dark:text-sky-400 font-bold">카키디</span> 입니다.
             </p>
-            <div className="flex gap-1 mt-3 pt-3">
+            <div className="flex gap-1 mt-3 pt-3 justify-end lg:justify-start">
               {" "}
               {/* border-t border-slate-100 dark:border-gray-500 */}
               <a
