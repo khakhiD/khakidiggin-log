229012a00776d6e864a6b4a466a81a0f5b588740
Dongho Shin <kakkiid@gmail.com>
2023-06-13 02:30:46 +0900

    feat: <TagList> responsive added 2

--- src/containers/Feed/index.tsx
+++ src/containers/Feed/index.tsx
@@ -38,7 +38,7 @@ const Feed: React.FC<Props> = ({ categories, tags, posts }) => {
       <div className="col-span-12 lg:col-span-7">
         <Cards.MobileProfileCard />
         <SearchInput value={q} onChange={(e) => setQ(e.target.value)} />
-        <Lists.TagList className="block lg:hidden" data={tags} />
+        <Lists.TagList className="flex w-[100%] h-auto lg:hidden" data={tags} />
         <FeedHeader categories={categories} />
         <Lists.PostList q={q} posts={posts} />
         <Footer className="block lg:hidden flex justify-center pb-8" />
