3bd2374746ecbfc3c7c6ea5b78d84cd5d10ccda9
khakhiD <kakkiid@gmail.com>
2024-05-22 01:05:44 +0900

    feat: 폰트 CSS 분리 및 새 폰트 Freesentation 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
-import 'prismjs/themes/prism.css'
-import 'react-notion-x/src/styles.css'
-import 'katex/dist/katex.min.css'
-import '@styles/globals.css'
-import '@styles/notion.css'
-import useThemeEffect from '@hooks/useThemeEffect'
-import useGtagEffect from '@hooks/useGtagEffect'
-import Scripts from '@components/Scripts'
-import { NextPage } from 'next'
-import { ReactElement, ReactNode } from 'react'
-import { AppProps } from 'next/app'
+import "prismjs/themes/prism.css"
+import "react-notion-x/src/styles.css"
+import "katex/dist/katex.min.css"
+import "@styles/globals.css"
+import "@styles/fonts.css"
+import "@styles/notion.css"
+import useThemeEffect from "@hooks/useThemeEffect"
+import useGtagEffect from "@hooks/useGtagEffect"
+import Scripts from "@components/Scripts"
+import { NextPage } from "next"
+import { ReactElement, ReactNode } from "react"
+import { AppProps } from "next/app"
 
 export type NextPageWithLayout<PageProps = {}> = NextPage<PageProps> & {
   getLayout?: (page: ReactElement) => ReactNode
