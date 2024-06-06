fc78d359f42ba2184c151826677b0f5843a58b43
khakhiD <kakkiid@gmail.com>
2024-06-06 23:49:35 +0900

    style: ProfileCard bio 표시 및 주석 제거

--- src/containers/Feed/components/cards/ProfileCard.tsx
+++ src/containers/Feed/components/cards/ProfileCard.tsx
@@ -9,13 +9,12 @@ type Props = {
 const ProfileCard: React.FC<Props> = ({ className }) => {
   return (
     <div className={className}>
-      {/* <div className="p-1 mb-3 dark:text-white">💻 Profile</div> */}
       <div className="w-full md:p-4 lg:p-4 mb-3">
         <div className="relative w-full rounded-3xl overflow-hidden after:content-[''] after:block after:pb-[100%] drop-shadow-sm">
           <Image
-                src="https://avatars.githubusercontent.com/u/74141521?v=4"
-                alt="profile-image"
-                layout="fill"
+            src="https://avatars.githubusercontent.com/u/74141521?v=4"
+            alt="profile-image"
+            layout="fill"
           />
         </div>
         <div className="p-2 flex flex-col items-center dark:text-white">
@@ -23,7 +22,7 @@ const ProfileCard: React.FC<Props> = ({ className }) => {
           <div className="text-sm text-center mb-4 text-gray-500 dark:text-gray-400">
             {CONFIG.profile.role}
           </div>
-          {/* <div className="text-sm mb-2 text-center">{CONFIG.profile.bio}</div> */}
+          <div className="text-sm mb-2 text-center">{CONFIG.profile.bio}</div>
         </div>
       </div>
     </div>
