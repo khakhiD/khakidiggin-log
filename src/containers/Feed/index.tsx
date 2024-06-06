18c748a8aabfa3a25879a2d8ca0c020e36362716
khakhiD <kakkiid@gmail.com>
2024-06-06 23:49:58 +0900

    style: 메인 페이지 Hero 숨김 및 주석 제거

--- src/containers/Feed/index.tsx
+++ src/containers/Feed/index.tsx
@@ -19,13 +19,6 @@ type Props = {
 const Feed: React.FC<Props> = ({ categories, tags, posts }) => {
   const [q, setQ] = useState("")
 
-  //   .box {
-  //     -ms-overflow-style: none;
-  //     scrollbar-width: none;
-  // }
-  // .box::-webkit-scrollbar {
-  //     display: none;
-  // }
   return (
     <>
       <div className="flex flex-col">
@@ -34,22 +27,19 @@ const Feed: React.FC<Props> = ({ categories, tags, posts }) => {
         </div> */}
 
         <div className="block md:grid md:grid-cols-12 gap-6">
-          <div className="mt-5 top-[200px] hidden lg:block col-span-2">
+          <div className="lg:mt-5 top-[200px] hidden lg:block col-span-2">
             <Cards.ProfileCard />
             <div
-              className="common-no-scroll-bar sticky mt-12 top-[81px] hidden lg:block overflow-scroll"
+              className="common-no-scroll-bar sticky mt-10 top-[81px] hidden lg:block overflow-scroll"
               style={{
                 height: "calc(100vh - 81px)",
               }}
             >
               <Lists.TagList data={tags} />
             </div>
-            {/* <Lists.CategoryList data={categories} /> */}
-
-            {/* <Cards.ContactCard /> */}
           </div>
-          <div className="col-span-12 lg:col-span-10 ">
-            <Hero className="flex w-[100%] h-auto hidden md:block md:flex-row pt-12 pb-12 w-[100%]" />
+          <div className="col-span-12 lg:col-span-10 lg:mt-10">
+            {/* <Hero className="flex w-[100%] h-auto hidden md:block md:flex-row pt-12 pb-12 w-[100%]" /> */}
             <Cards.MobileProfileCard />
             <SearchInput value={q} onChange={(e) => setQ(e.target.value)} />
             <Lists.TagList
@@ -58,7 +48,7 @@ const Feed: React.FC<Props> = ({ categories, tags, posts }) => {
             />
             <FeedHeader categories={categories} />
             <Lists.PostList q={q} posts={posts} />
-            <Footer className="block flex justify-center pb-8" />
+            <Footer className="flex justify-center pb-8" />
           </div>
         </div>
       </div>
